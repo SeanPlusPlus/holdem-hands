3948649f73fab8c2f6a720a6fad13224ac750d17
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2022-10-21 13:59:31 -0700

    main: simple

--- components/home.js
+++ components/home.js
@@ -79,7 +79,7 @@ const Home = () => {
                   key={n}
                   id={n}
                   onClick={handleClick}
-                  className="btn btn-lg border rounded border-sky-500 text-xl font-bold cursor-pointer md:hover:bg-base-300"
+                  className="btn btn-lg border rounded border-base-300 text-xl font-bold cursor-pointer"
                 >{n}</div>
               ))}
             </div>
