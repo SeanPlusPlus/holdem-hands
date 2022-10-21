3c7c5d73a60a9efdf167a36f53d18b70d8b881b0
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2022-10-21 13:16:47 -0700

    main: card / suit

--- components/home.js
+++ components/home.js
@@ -25,25 +25,23 @@ const Home = () => {
       <div className="hero bg-base-200">
         <div className="hero-content text-center mb-64 mt-1 ml-0 mr-0 pr-0 pl-0">
           <div className="max-w-md">
-            <div className="flex">
+            <div className="flex mb-8">
               <div className="mr-2">
                 <Image
-                  src="/cards/KS.svg"
-                  alt="ks"
+                  src={`/cards/${display[0].card}${display[0].suit}.svg`}
+                  alt={display[0].card + display[0].suit}
                   width={160}
                   height={230}
                 />
               </div>
               <div className="ml-2">
                 <Image
-                  src="/cards/QS.svg"
-                  alt="qs"
+                  src={`/cards/${display[1].card}${display[1].suit}.svg`}
+                  alt={display[1].card + display[1].suit}
                   width={160}
                   height={230}
                 />
-
               </div>
-
             </div>
             {display[0].card} of {display[0].suit}
             <br />
--- utils/deck.js
+++ utils/deck.js
@@ -1,34 +1,38 @@
 const getFaceCard = (num) => {
+  if (num === 10) {
+    return 'T'
+  }
+
   if (num === 11) {
-    return 'Jack'
+    return 'J'
   }
   
   if (num === 12) {
-    return 'Queen'
+    return 'Q'
   }
 
   if (num === 13) {
-    return 'King'
+    return 'K'
   }
 
   if (num === 14) {
-    return 'Ace'
+    return 'A'
   }
 }
 
 export const deck = () => {
   const cards = []
   const suits = [
-    'Hearts',
-    'Spades',
-    'Diamonds',
-    'Clubs',
+    'H',
+    'S',
+    'D',
+    'C',
   ]
   const uniq = 13
   suits.forEach((suit) => {
     for (var i = 0; i < uniq; i++) {
       const num = i + 2
-      const card = (num > 10) ? getFaceCard(num) : num
+      const card = (num > 9) ? getFaceCard(num) : num
       cards.push({
         card,
         suit,
