57e6c3a0ddd53e4d468c7cfd5462f2c9f1d0d5b9
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2022-10-21 12:44:16 -0700

    main: more sky in icon

--- components/nav.js
+++ components/nav.js
@@ -26,10 +26,10 @@ const Nav = () => {
         </div>
         <div className="flex-none gap-2">
           <button
-            className="mr-2 btn btn-outline sky-50 border-sky-50"
+            className="mr-2 btn btn-outline border-sky-50"
             onClick={handleOpenAbout}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 sky-50">
               <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
             </svg>
 
