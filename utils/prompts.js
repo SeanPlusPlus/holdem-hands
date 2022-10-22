97da99bd497b0c1c12cf93f0421755c9e27cfa79
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2022-10-21 17:58:26 -0700

    main: ten prompts

--- utils/prompts.js
+++ utils/prompts.js
@@ -3,4 +3,10 @@ export const prompts = [
   'Cards are war in disguise of a sport',
   'Poker is war. People pretend it is a game',
   'Show me a good loser and I\'ll show you a loser.',
+  'In the long run there’s no luck in poker',
+  'May the flop be with you',
+  'A hard way to make an easy living',
+  'Money won is twice as sweet as money earned',
+  'The smarter you play, the luckier you’ll be',
+  'Takes a minute to learn but a lifetime to master',
 ]
