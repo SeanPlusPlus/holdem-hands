2ac67f823f4119e1c1288aaa70b32ef9145655ee
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2022-10-21 17:24:42 -0700

    main: update header desc

--- components/header.js
+++ components/header.js
@@ -11,13 +11,13 @@ const Header = () => {
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://holdem-hands.vercel.app/" />
       <meta property="og:title" content="Hold&apos;em Hands" />
-      <meta property="og:description" content="Texas Hold&apos;em Tutorial" />
+      <meta property="og:description" content="Texas Hold&apos;em Tutorial by Sean++" />
       <meta property="og:image" content="https://i.imgur.com/9mRsAsh.png" />
       
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:domain" value="holdem-hands.vercel.app" />
       <meta name="twitter:title" value="Hold&apos;em Hands" />
-      <meta name="twitter:description" value="Texas Hold&apos;em Tutorial" />
+      <meta name="twitter:description" value="Texas Hold&apos;em Tutorial by Sean++" />
       <meta name="twitter:image" content="https://i.imgur.com/9mRsAsh.png" />
       <meta name="twitter:url" value="https://holdem-hands.vercel.app/" />
     </Head>
