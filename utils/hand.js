0724ef7b7e85e76475218ae5edb5b0f136cb6dd9
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2022-10-21 20:17:25 -0700

    main: comment

--- utils/hand.js
+++ utils/hand.js
@@ -4,16 +4,16 @@ import _sampleSize from 'lodash/sampleSize'
 export const hand = () => {
   const rand = _sampleSize(deck(), 2)
 
-  return [
-    {
-      card: 'K',
-      suit: 'C'
-    },
-    {
-      card: 'K',
-      suit: 'S'
-    },
-  ]
+  // return [
+  //   {
+  //     card: 'K',
+  //     suit: 'C'
+  //   },
+  //   {
+  //     card: 'K',
+  //     suit: 'S'
+  //   },
+  // ]
 
   return rand
 }
