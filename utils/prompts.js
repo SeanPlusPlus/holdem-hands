1f70f800a84e973231a10b78752ca3500cec51fe
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2022-10-21 17:55:58 -0700

    main: prompts

---
+++ utils/prompts.js
@@ -0,0 +1,6 @@
+export const prompts = [
+  'The one who bets the most wins',
+  'Cards are war in disguise of a sport',
+  'Poker is war. People pretend it is a game',
+  'Show me a good loser and I\'ll show you a loser.',
+]
