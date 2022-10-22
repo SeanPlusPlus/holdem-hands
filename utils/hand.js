9de0fafb0e0fcdc992eb7bf1f9f3ebdc974b00d0
SeanPlusPlus <seanmichaelstephenson@gmail.com>
2022-10-21 17:09:20 -0700

    main: better test obj

--- utils/hand.js
+++ utils/hand.js
@@ -4,30 +4,17 @@ import _sampleSize from 'lodash/sampleSize'
 export const hand = () => {
   const rand = _sampleSize(deck(), 2)
 
-  // Ace
-  // return [{card: 'A', suit: rand[0].suit}, rand[1]]
+  // return [
+  //   {
+  //     card: '9',
+  //     suit: 'C'
+  //   },
+  //   {
+  //     card: 'A',
+  //     suit: 'S'
+  //   },
+  // ]
 
-  // King
-  // return [{card: 'K', suit: rand[0].suit}, rand[1]]
-
-  // Queen
-  // return [{card: 'Q', suit: rand[0].suit}, rand[1]]
-
-  // Jack 
-  // return [{card: 'J', suit: rand[0].suit}, rand[1]]
-
-  // Ten
-  // return [{card: 'T', suit: rand[0].suit}, rand[1]]
-
-  // Nine
-  // return [{card: '9', suit: rand[0].suit}, rand[1]]
-
-  // Eight
-  // return [{card: '8', suit: rand[0].suit}, rand[1]]
-
-  // Seven
-  // return [{card: '7', suit: rand[0].suit}, rand[1]]
-  
   return rand
 }
 
